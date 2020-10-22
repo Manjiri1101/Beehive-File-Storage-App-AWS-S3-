82507136f5c13acb18817eb6e4c8978a17b1e1b5
Manjiri 1101 <Manjiri1101@users.noreply.github.com>
2020-10-21 21:36:55 -0700

    Added Authentication

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Amplify,{Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
 
+
+Amplify.configure(awsconfig)
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AmplifySignOut />
+        <h2>Beehive</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
